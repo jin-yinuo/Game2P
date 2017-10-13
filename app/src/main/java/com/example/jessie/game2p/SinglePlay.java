be794a46e432866d516adf9581a72a96b6ff6c6b
Jessie <y69jin@edu.uwaterloo.ca>
2017-10-14 01:06:42 +0900

    xml file

--- app/src/main/java/com/example/jessie/game2p/SinglePlay.java
+++ app/src/main/java/com/example/jessie/game2p/SinglePlay.java
@@ -1,13 +1,17 @@
 package com.example.jessie.game2p;
 
+/**
+ * Created by Jessie on 10/14/2017.
+ */
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Main extends AppCompatActivity {
+public class SinglePlay extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
