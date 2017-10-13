d34f536c3ca2f82e19a59459da3550fbcefe1497
Jessie <y69jin@edu.uwaterloo.ca>
2017-10-13 23:23:00 +0900

    adding hello and variable a

--- app/src/main/java/com/example/jessie/game2p/Main.java
+++ app/src/main/java/com/example/jessie/game2p/Main.java
@@ -1,9 +1,10 @@
 package com.example.jessie.game2p;
-
+//hellooooooooooooooooooooooooooooooo
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Main extends AppCompatActivity {
+    public int a = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
