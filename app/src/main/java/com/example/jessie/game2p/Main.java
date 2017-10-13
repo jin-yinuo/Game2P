676d1945d08a74f84bfb5c72db465cdf31006494
Jessie <y69jin@edu.uwaterloo.ca>
2017-10-13 23:33:02 +0900

    a = 5

--- app/src/main/java/com/example/jessie/game2p/Main.java
+++ app/src/main/java/com/example/jessie/game2p/Main.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Main extends AppCompatActivity {
-    public int a = 4;
+    public int a = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
