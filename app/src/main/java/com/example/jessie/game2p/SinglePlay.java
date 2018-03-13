3a38d70bffc602353609089e1dff8b5c911ae847
Adriena Wong <atwong88@gmail.com>
2018-03-12 22:24:28 -0400

    changed the buttons to imageviews and tried to change their colours lol

--- app/src/main/java/com/example/jessie/game2p/SinglePlay.java
+++ app/src/main/java/com/example/jessie/game2p/SinglePlay.java
@@ -31,10 +31,10 @@ public class SinglePlay extends AppCompatActivity {
     ImageView pauseScreenBackground;
     Button resumeButton;
     Button mainmenuButton;
-    Button b11, b12, b13, b14, b21, b22, b23, b24, b31, b32, b33, b34, b41, b42, b43, b44;
-    ImageView i11, i12, i13, i14, i21, i22, i23, i24, i31, i32, i33, i34, i41, i42, i43, i44;
-    Button[][] bArray = {{b11, b12, b13, b14}, {b21, b22, b23, b24}, {b31, b32, b33, b34}, {b41, b42, b43, b44}};
-    ImageView[][] sqArray = {{i11, i12, i13, i14}, {i21, i22, i23, i24}, {i31, i32, i33, i34}, {i41, i42, i43, i44}};
+    ImageView i11, i12, i13, i14, i21, i22, i23, i24, i31, i32, i33, i34, i41, i42, i43, i44,
+            i51, i52, i53, i54, i61, i62, i63, i64, i71, i72, i73, i74, i81, i82, i83, i84;
+    ImageView[][] sqArray = {{i11, i12, i13, i14}, {i21, i22, i23, i24}, {i31, i32, i33, i34}, {i41, i42, i43, i44},
+            {i51, i52, i53, i54}, {i61, i62, i63, i64}, {i71, i72, i73, i74}, {i81, i82, i83, i84}};
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,22 +48,38 @@ public class SinglePlay extends AppCompatActivity {
         pauseScreenBackground = (ImageView) findViewById(R.id.pause_screen_background);
         resumeButton = (Button) findViewById(R.id.resume_button);
         mainmenuButton = (Button) findViewById(R.id.main_menu_button);
-        b11 = (Button) findViewById(R.id.button11);
-        b12 = (Button) findViewById(R.id.button12);
-        b13 = (Button) findViewById(R.id.button13);
-        b14 = (Button) findViewById(R.id.button14);
-        b21 = (Button) findViewById(R.id.button21);
-        b22 = (Button) findViewById(R.id.button22);
-        b23 = (Button) findViewById(R.id.button23);
-        b24 = (Button) findViewById(R.id.button24);
-        b31 = (Button) findViewById(R.id.button31);
-        b32 = (Button) findViewById(R.id.button32);
-        b33 = (Button) findViewById(R.id.button33);
-        b34 = (Button) findViewById(R.id.button34);
-        b41 = (Button) findViewById(R.id.button41);
-        b42 = (Button) findViewById(R.id.button42);
-        b43 = (Button) findViewById(R.id.button43);
-        b44 = (Button) findViewById(R.id.button44);
+        i11 = (ImageView) findViewById(R.id.imageView11);
+        i12 = (ImageView) findViewById(R.id.imageView12);
+        i13 = (ImageView) findViewById(R.id.imageView13);
+        i14 = (ImageView) findViewById(R.id.imageView14);
+        i21 = (ImageView) findViewById(R.id.imageView21);
+        i22 = (ImageView) findViewById(R.id.imageView22);
+        i23 = (ImageView) findViewById(R.id.imageView23);
+        i24 = (ImageView) findViewById(R.id.imageView24);
+        i31 = (ImageView) findViewById(R.id.imageView31);
+        i32 = (ImageView) findViewById(R.id.imageView32);
+        i33 = (ImageView) findViewById(R.id.imageView33);
+        i34 = (ImageView) findViewById(R.id.imageView34);
+        i41 = (ImageView) findViewById(R.id.imageView41);
+        i42 = (ImageView) findViewById(R.id.imageView42);
+        i43 = (ImageView) findViewById(R.id.imageView43);
+        i44 = (ImageView) findViewById(R.id.imageView44);
+        i51 = (ImageView) findViewById(R.id.imageView51);
+        i52 = (ImageView) findViewById(R.id.imageView52);
+        i53 = (ImageView) findViewById(R.id.imageView53);
+        i54 = (ImageView) findViewById(R.id.imageView54);
+        i61 = (ImageView) findViewById(R.id.imageView61);
+        i62 = (ImageView) findViewById(R.id.imageView62);
+        i63 = (ImageView) findViewById(R.id.imageView63);
+        i64 = (ImageView) findViewById(R.id.imageView64);
+        i71 = (ImageView) findViewById(R.id.imageView71);
+        i72 = (ImageView) findViewById(R.id.imageView72);
+        i73 = (ImageView) findViewById(R.id.imageView73);
+        i74 = (ImageView) findViewById(R.id.imageView74);
+        i81 = (ImageView) findViewById(R.id.imageView81);
+        i82 = (ImageView) findViewById(R.id.imageView82);
+        i83 = (ImageView) findViewById(R.id.imageView83);
+        i84 = (ImageView) findViewById(R.id.imageView84);
 
 
         pauseButton.setOnClickListener(new View.OnClickListener() {
@@ -73,9 +89,9 @@ public class SinglePlay extends AppCompatActivity {
                 resumeButton.setVisibility(View.VISIBLE);
                 mainmenuButton.setVisibility(View.VISIBLE);
                 pauseButton.setEnabled(false);
-                for (int i = 0; i < 4; ++i) {
+                for (int i = 0; i < 8; ++i) {
                     for (int j = 0; j < 4; ++j) {
-//                        bArray[i][j].setEnabled(false);
+//                        sqArray[i][j].setEnabled(false);
                     }
                 }
             }
@@ -88,9 +104,9 @@ public class SinglePlay extends AppCompatActivity {
                 resumeButton.setVisibility(View.GONE);
                 mainmenuButton.setVisibility(View.GONE);
                 pauseButton.setEnabled(true);
-                for (int i = 0; i < 4; ++i) {
+                for (int i = 0; i < 8; ++i) {
                     for (int j = 0; j < 4; ++j) {
-//                        bArray[i][j].setEnabled(true);
+//                        sqArray[i][j].setEnabled(true);
                     }
                 }
             }
@@ -103,6 +119,18 @@ public class SinglePlay extends AppCompatActivity {
             }
         });
 
+//        for(int i = 0; i < 8; i++){
+//            for(int j = 0; j < 4; j++){
+//                final int x = i;
+//                final int y = j;
+//                sqArray[i][j].setOnClickListener(new View.OnClickListener() {
+//                    public void onClick(View v) {
+//                        toggleSquare(sqArray[x][y]);
+//                    }
+//                });
+//            }
+//        }
+
         Button shuffleButton = (Button) findViewById(R.id.shuffle_button);
         shuffleButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -112,13 +140,12 @@ public class SinglePlay extends AppCompatActivity {
         });
     }
 
-
     public void toggleSquare(ImageView square) {
         if (square.isSelected()) {
-            square.setBackgroundColor(Color.BLUE);
+            square.setColorFilter(Color.BLUE);
             square.setSelected(false);
         } else {
-            square.setBackgroundColor(Color.RED);
+            square.setColorFilter(Color.RED);
             square.setSelected(true);
         }
     }
@@ -128,14 +155,14 @@ public class SinglePlay extends AppCompatActivity {
         int coloured = obj.nextInt(5) + 5;
         int[] randomArr = new int[coloured];
         for (int i = 0; i < coloured; ++i) {
-            int r = obj.nextInt(16);
+            int r = obj.nextInt(32);
             if (!contains(randomArr, r)) {
                 randomArr[i] = r;
                 break;
             }
         }
         for (int i = 0; i < coloured; ++i) {
-            int row = randomArr[i] / 4;
+            int row = randomArr[i] / 8;
             int col = randomArr[i] % 4;
 //            ImageView sq = sqArray[row][col];
 //            toggleSquare(sq);
